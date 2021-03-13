0369a4e1a83371cccb2cb90fa7ba6e3c992305ff
kadaxda <Dr.J4kob@gmail.com>
2021-03-13 17:46:09 +0100

    Logic is implemented

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 //Selectors
+const display = document.querySelector("#display")
 
+//Player factory
+let Player = function(name, logo){
+    this.name = name;
+    this.logo = logo;
+    return {name, logo}
+}
+
+//Game
+let game = function() {
+
+    Player1 = Player("Human", "X");
+    Player2 = Player("PC", "O")
+    
+    return {Player1, Player2}
+}();
 
 
 //Gameboard module
 let gameboard = (function() {
 
-    let currentTurn = "X"
+    let currentTurn = Player1.logo
 
-    let myBoard = ["X", "", "",
+    let myBoard = [ "", "", "",
                     "", "", "",
-                    "", "A", ""]
+                    "", "", ""]
 
     function renderMyBoard() {
         let fields = document.querySelectorAll(".field");
@@ -22,8 +38,33 @@ let gameboard = (function() {
     fields.forEach((field) => {
         field.addEventListener("click", (e) => {
             if(field.textContent == "") {
-                field.textContent = currentTurn;
-                changeTurn();            
+                if(field.classList.contains("top-l")) {
+                    myBoard[0] = currentTurn;
+                } else if(field.classList.contains("top-m")) {
+                    myBoard[1] = currentTurn;
+                } else if(field.classList.contains("top-r")) {
+                    myBoard[2] = currentTurn;
+                }
+
+                else if(field.classList.contains("mid-l")) {
+                    myBoard[3] = currentTurn;
+                } else if(field.classList.contains("mid-m")) {
+                    myBoard[4] = currentTurn;
+                } else if(field.classList.contains("mid-r")) {
+                    myBoard[5] = currentTurn;
+                }
+
+                else if(field.classList.contains("bot-l")) {
+                    myBoard[6] = currentTurn;
+                } else if(field.classList.contains("bot-m")) {
+                    myBoard[7] = currentTurn;
+                } else if(field.classList.contains("bot-r")) {
+                    myBoard[8] = currentTurn;
+                }
+                console.log(myBoard)
+                renderMyBoard();
+                changeTurn();   
+                checkIfWon();         
             }
         })
     })
@@ -36,12 +77,73 @@ let gameboard = (function() {
         };
     }
 
-    return {renderMyBoard}
+    function checkIfWon() {
+        if(myBoard[0] == myBoard[1] && myBoard[1] == myBoard[2] && myBoard[2] != "") {
+            if(myBoard[0] == "X") {
+                winningPlayer = Player1;
+            } else {
+                winningPlayer = Player2;
+            }
+            display.textContent = `The Winner is ${winningPlayer.name} ${winningPlayer.logo}`
+        } else if(myBoard[3] == myBoard[4] && myBoard[4] == myBoard[5] && myBoard[5] != "") {
+            if(myBoard[3] == "X") {
+                winningPlayer = Player1;
+            } else {
+                winningPlayer = Player2;
+            }
+            display.textContent = `The Winner is ${winningPlayer.name} ${winningPlayer.logo}`
+        } else if(myBoard[6] == myBoard[7] && myBoard[7] == myBoard[8] && myBoard[8] != "") {
+            if(myBoard[6] == "X") {
+                winningPlayer = Player1;
+            } else {
+                winningPlayer = Player2;
+            }
+            display.textContent = `The Winner is ${winningPlayer.name} ${winningPlayer.logo}`
+        } 
+        
+        
+        else if(myBoard[0] == myBoard[3] && myBoard[3] == myBoard[6] && myBoard[6] != "") {
+            if(myBoard[0] == "X") {
+                winningPlayer = Player1;
+            } else {
+                winningPlayer = Player2;
+            }
+            display.textContent = `The Winner is ${winningPlayer.name} ${winningPlayer.logo}`
+        } else if(myBoard[1] == myBoard[4] && myBoard[4] == myBoard[7] && myBoard[7] != "") {
+            if(myBoard[1] == "X") {
+                winningPlayer = Player1;
+            } else {
+                winningPlayer = Player2;
+            }
+            display.textContent = `The Winner is ${winningPlayer.name} ${winningPlayer.logo}`
+        } else if(myBoard[2] == myBoard[5] && myBoard[5] == myBoard[8] && myBoard[8] != "") {
+            if(myBoard[2] == "X") {
+                winningPlayer = Player1;
+            } else {
+                winningPlayer = Player2;
+            }
+            display.textContent = `The Winner is ${winningPlayer.name} ${winningPlayer.logo}`
+        } 
+
+        else if(myBoard[0] == myBoard[4] && myBoard[4] == myBoard[8] && myBoard[8] != "") {
+            if(myBoard[0] == "X") {
+                winningPlayer = Player1;
+            } else {
+                winningPlayer = Player2;
+            }
+            display.textContent = `The Winner is ${winningPlayer.name} ${winningPlayer.logo}`
+        } else if(myBoard[2] == myBoard[4] && myBoard[4] == myBoard[6] && myBoard[6] != "") {
+            if(myBoard[2] == "X") {
+                winningPlayer = Player1;
+            } else {
+                winningPlayer = Player2;
+            }
+            display.textContent = `The Winner is ${winningPlayer.name} ${winningPlayer.logo}`
+        }
+    }
+    
+
+    return {renderMyBoard, myBoard}
 })();
 
-//Player factory
-let Player = function(name, logo){
-    return {name, logo}
-}
 
-gameboard.renderMyBoard();
